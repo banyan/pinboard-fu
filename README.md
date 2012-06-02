8ea5b66e3f21328d91776c5d4fcc36100956b59b
Kohei Hasegawa <ameutau@gmail.com>
2012-06-02 22:47:11 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# pinboard-fu
+
+## Description
+
+Port of https://github.com/billkeller/Delicious-Chrome-Extension from delicious to pinboard.
+
+1. Bookmark with popup
+1. Available by shortcut key (eg: Alt-D)
+1. Selected text populates your description field.
+
+## Contributing
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Added some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
---
+++ addPinboard.js
@@ -0,0 +1,56 @@
+(function() {
+  var localSettings = {
+    bookmarkKeyChar:    'D',
+    bookmarkSpecialKey: 'alt'
+  };
+
+  function getVariableFromLocalStorage(variableName, defaultValue) {
+    localSettings[variableName] = defaultValue;
+    var onResponse = function (response) {
+      if (response !== null) {
+        localSettings[variableName] = response;
+      }
+    }
+    chrome.extension.sendRequest({'action': 'getFromLocalStorage', 'variableName': variableName}, onResponse);
+  }
+
+  getVariableFromLocalStorage('bookmarkKeyChar', localSettings.bookmarkKeyChar);
+  getVariableFromLocalStorage('bookmarkSpecialKey', localSettings.bookmarkSpecialKey);
+
+	// Listen for key press
+  window.addEventListener(
+    'keydown',
+    function(e) {
+      if ((e.which == localSettings.bookmarkKeyChar.charCodeAt(0))
+        && (
+          (localSettings.bookmarkSpecialKey == 'alt' && e.altKey)
+          || (localSettings.bookmarkSpecialKey == 'ctrl' && e.ctrlKey)
+          || (localSettings.bookmarkSpecialKey == 'meta' && e.metaKey)
+      )) {
+        // prevent the default, doesn't seem to overwrite the windows (Meta) key
+        e.preventDefault();
+        addPinboardFromContentScript();
+      }
+    },
+    false
+  );
+
+  addPinboardFromContentScript = function() {
+    var url = document.location.toString(),
+      title = document.title,
+      description = '';
+
+    if (window && window.getSelection) {
+      description = window.getSelection().toString();
+    } else if (document && document.getSelection) {
+      description = document.getSelection().toString();
+    }
+
+    chrome.extension.sendRequest({
+      type:  'addPinboard',
+      url:   url,
+      title: title,
+      description: description
+    });
+  };
+})();
